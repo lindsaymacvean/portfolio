20592107db29ddc07a164492d0fcc7bac152ea24
lindsay macvean <lindsaymacvean@gmail.com>
2018-03-04 01:47:22 +0000

    got the debian ubuntu installation package build system working

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 A Fedora 27 installation package (possibly Centos 6) is located in centos-fedora-package/noarch/
 Run `sudo dnf install centos-fedora-package/noarch/invotra-process-tracker-1-1.fc27.noarch.rpm`
 
-A Debian Ubuntu 14+ package is located in 
+A Debian Ubuntu 14+ package is located in debian-ubuntu-package
+Run `sudo dpkg -i debian-ubuntu-package/invotra-process-tracker_1.0_all.deb`
+And to check dependendencies are clean `sudo apt-get install -f .`
 
 
 The Java App is located in java-src
@@ -17,4 +19,4 @@ Second run `sudo ./build.sh` to create the installation packages
 
 ## Uninstall
 `sudo dnf remove invotra-process-tracker`
-`sudo apt uninstall invotra-process-tracker`
+`sudo apt-get remove invotra-process-tracker`
--- build.sh
+++ build.sh
@@ -10,13 +10,14 @@ fedpkg --release f27 local
 popd
 # Further work required on how to create upgrade package
 # https://www.ibm.com/developerworks/library/l-rpm2/
-exit
 
 # Build a Debiuan Ubuntu package
 echo "Building a Debian/Ubuntu package"
 cp java-src/out/artifacts/process_tracker_jar/process_tracker.jar debian-ubuntu-package/usr/local/bin
-cp systemd-service/invotracker.service centos-fedora-package
+chmod +x debian-ubuntu-package/usr/local/bin/process_tracker.jar
+cp systemd-service/invotracker.service debian-ubuntu-package/etc/systemd/system
 pushd debian-ubuntu-package
-dpkg-deb --build debian-process-tracker
+# We need the two stops to build from 'current directory' to 'current directory'
+dpkg-deb --build . .
 popd
 # Install on debian machine using sudo apt install ./name.deb
